343056c2687a4b8e809d4e6b17aa57b159028870
Fioreza Radhin Naufal <122506640+fiorezarn1902@users.noreply.github.com>
2023-04-07 22:32:20 +0700

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,34 +6,40 @@ import "./component/restaurant-footer.js";
 document.addEventListener("DOMContentLoaded", async () => {
   const jsonData = await import('../DATA.json');
   const datas = jsonData.default.restaurants;
-  let dataList = '';
+  let dataCard = '';
   datas.forEach(function(data) {
-      dataList +=`
-      <div class="list_item">
-          <img class="list_item_thumb" src="${data.pictureId}" alt="${data.name}" title="${data.name}">
-          <div class="city">${data.city}</div>
-          <div class="list_item_content">
-              <p class="list_item_rating">
+      dataCard +=`
+      <div class="card">
+          <img class="card_thumb" src="${data.pictureId}" alt="${data.name}" title="${data.name}">
+          <div class="kota">${data.city}</div>
+          <div class="card_content">
+              <p class="card_rating">
                   Rating : 
-                  <a href="#" class="list_item_rating_value">${data.rating}</a>
+                  <a href="#" class="card_rating_number">${data.rating}</a>
               </p>
-              <h1 class="list_item_title"><a href="#">${data.name}</a></h1>
-              <div class="list_item_desc">${data.description.slice(0, 150)}...</div>
+              <h1 class="card_title"><a href="#">${data.name}</a></h1>
+              <div class="card_desc">${data.description.slice(0, 150)}...</div>
           </div>
       </div>
       `;
   });
-  document.querySelector('#tes').innerHTML = dataList;
+  document.querySelector('#tes').innerHTML = dataCard;
+});
 
-  const hero = document.querySelector('.hero');
-  const main = document.querySelector('main');
-  const drawer = document.querySelector('#drawer');
-
-  hero.addEventListener('click', function () {
-      drawer.classList.remove('open');
-  });
-
-  main.addEventListener('click', function () {
-      drawer.classList.remove('open');
-  });
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdown = document.querySelector('.dropdown')
+    const kanan = document.querySelector('.kanan')
+    const sisikanan = document.querySelector('.sisi-kanan')
+    
+    dropdown.addEventListener('click', function() {
+      this.classList.toggle('ubah')
+      kanan.classList.toggle('ubah')
+      sisikanan.classList.toggle('ubah')
+    })
+    
+    sisikanan.addEventListener('click', function() {
+      this.classList.remove('ubah')
+      kanan.classList.remove('ubah')
+      dropdown.classList.remove('ubah')
+    })
 });
